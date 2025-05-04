b0f3fe201b5c10b147c61ab75d047d15c30ea468
Прокопьев Денис <Prokopev-dd-js@yandex.ru>
2025-05-04 10:27:03 +0300

    Маршрут по тайным тропам

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import React from 'react';
-import MainPage from './components/MainPage/MainPage';
+import MainPage from './components/MainPage/main-page';
 import { offers } from './constants';
+import NotFoundPage from './components/NotFoundPage/not-found-page';
+import FavoritesPage from './components/FavoritesPage/favorite-page';
+import LoginPage from './components/LoginPage/login-page';
+import OfferPage from './components/OfferPage/offer-page';
+import PrivateRoute from './components/PrivateRoute/private-route';
+
 
 const App: React.FC = () => (
-  <MainPage offersCount={offers.length} />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage offers={offers} />} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/favorites" element={
+        <PrivateRoute isAuthorized={false}>
+          <FavoritesPage />
+        </PrivateRoute>
+      }
+      />
+      <Route path="/offer/:id" element={<OfferPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
